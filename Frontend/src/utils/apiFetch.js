6af44d9ebbe372443cc4f9c28ce586cb7903e7da
Dhruv <kachhiyadhruv61@gmail.com>
2026-06-25 12:15:44 +0530

    Update API URL

--- Frontend/src/utils/apiFetch.js
+++ Frontend/src/utils/apiFetch.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_URL;
 
 export const apiFetch = async (url, options = {}) => {
   let accessToken = localStorage.getItem("accessToken");
